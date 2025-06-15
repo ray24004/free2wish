7ba245a5ac8c1aaf13915d69db52bdd567889260
ray24004 <wraymundo@byupathway.edu>
2025-06-14 22:14:25 -0300

    Run format and lint

--- src/home/main.js
+++ src/home/main.js
@@ -8,25 +8,24 @@ let gameList = [];
 let gamesStart = 0;
 let gamesCount = 6;
 
-fetchGames()
-.then(games => {
-    gameList = games;
-    loadMoreGames();
+fetchGames().then((games) => {
+  gameList = games;
+  loadMoreGames();
 });
 
 function loadMoreGames() {
-    let gamesPage = gameList.slice(gamesStart, gamesStart + gamesCount);
-    for (const game of gamesPage) {
-      const gameItem = gameTemplate.content.cloneNode(true);
-    
-      gameItem.querySelector('h2').innerHTML = game.title;
-      gameItem.querySelector('img').src = game.thumbnail;
-      gameItem.querySelector('.year').innerHTML = game.release_date.split('-')[0];
-      gameItem.querySelector('.genre').innerHTML = game.genre;
-    
-      gameGrid.appendChild(gameItem);
-    }
-    gamesStart += gamesCount;
+  let gamesPage = gameList.slice(gamesStart, gamesStart + gamesCount);
+  for (const game of gamesPage) {
+    const gameItem = gameTemplate.content.cloneNode(true);
+
+    gameItem.querySelector('h2').innerHTML = game.title;
+    gameItem.querySelector('img').src = game.thumbnail;
+    gameItem.querySelector('.year').innerHTML = game.release_date.split('-')[0];
+    gameItem.querySelector('.genre').innerHTML = game.genre;
+
+    gameGrid.appendChild(gameItem);
+  }
+  gamesStart += gamesCount;
 }
 
 loadGamesButton.addEventListener('click', loadMoreGames);
