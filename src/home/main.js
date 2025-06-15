fc361b4620871146fc965667fa444e84a18e5205
ray24004 <wraymundo@byupathway.edu>
2025-06-14 22:13:28 -0300

    Separate api call to a module

--- src/home/main.js
+++ src/home/main.js
@@ -1,28 +1,21 @@
+import { fetchGames } from '../scripts/free-to-play';
+
 const gameGrid = document.querySelector('#gameGrid');
 const gameTemplate = document.querySelector('#gameTemplate');
 const loadGamesButton = document.querySelector('#loadGamesButton');
 
-async function fetchGames() {
-  const url = 'https://free-to-play-games-database.p.rapidapi.com/api/games';
-  const options = {
-    method: 'GET',
-    headers: {
-      'x-rapidapi-key': 'da5a0a4fbcmshbca012d0afd7b48p13dd3djsnd8c3217ce1e6',
-      'x-rapidapi-host': 'free-to-play-games-database.p.rapidapi.com',
-    },
-  };
-
-  const response = await fetch(url, options);
-  return await response.json();
-}
-
-const games = await fetchGames();
-
+let gameList = [];
 let gamesStart = 0;
 let gamesCount = 6;
 
+fetchGames()
+.then(games => {
+    gameList = games;
+    loadMoreGames();
+});
+
 function loadMoreGames() {
-    let gamesPage = games.slice(gamesStart, gamesStart + gamesCount);
+    let gamesPage = gameList.slice(gamesStart, gamesStart + gamesCount);
     for (const game of gamesPage) {
       const gameItem = gameTemplate.content.cloneNode(true);
     
@@ -35,7 +28,5 @@ function loadMoreGames() {
     }
     gamesStart += gamesCount;
 }
-
-loadMoreGames();
 
 loadGamesButton.addEventListener('click', loadMoreGames);
