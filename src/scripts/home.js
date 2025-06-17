dc848178b5707af9b55de6494b773652d87b114b
ray24004 <wraymundo@byupathway.edu>
2025-06-17 03:43:12 -0300

    Add title search on games feed

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -3,30 +3,53 @@ import freeToPlay from './modules/freeToPlay.js';
 const gameGrid = document.querySelector('#gameGrid');
 const gameTemplate = document.querySelector('#gameTemplate');
 const loadGamesButton = document.querySelector('#loadGamesButton');
+const searchInput = document.querySelector('#search');
 
 let gameList = [];
+let filteredGameList = [];
 let gamesStart = 0;
 let gamesCount = 6;
 
 freeToPlay.getGames().then((games) => {
   gameList = games;
+  filterResults();
   loadMoreGames();
 });
 
+loadGamesButton.addEventListener('click', loadMoreGames);
+
 function loadMoreGames() {
-  let gamesPage = gameList.slice(gamesStart, gamesStart + gamesCount);
-  for (const game of gamesPage) {
+  const gamesEnd = gamesStart + gamesCount;
+  const gamesBatch = filteredGameList.slice(gamesStart, gamesEnd);
+
+  for (const game of gamesBatch) {
     const gameItem = gameTemplate.content.cloneNode(true);
 
     gameItem.querySelector('h2').innerHTML = game.title;
     gameItem.querySelector('img').src = game.thumbnail;
     gameItem.querySelector('.year').innerHTML = game.release_date.split('-')[0];
     gameItem.querySelector('.genre').innerHTML = game.genre;
-    gameItem.querySelector('a').href = `/game?id=${game.id}`
+    gameItem.querySelector('a').href = `/game?id=${game.id}`;
 
     gameGrid.appendChild(gameItem);
   }
   gamesStart += gamesCount;
+
+  loadGamesButton.hidden = gamesEnd >= gamesBatch.length;
 }
 
-loadGamesButton.addEventListener('click', loadMoreGames);
+searchInput.addEventListener('input', filterResults);
+
+function filterResults() {
+  if (searchInput.value === '') {
+    filteredGameList = gameList;
+  } else {
+    filteredGameList = gameList.filter((g) =>
+      g.title.toLowerCase().includes(searchInput.value.toLowerCase()),
+    );
+  }
+
+  gameGrid.innerHTML = '';
+  gamesStart = 0;
+  loadMoreGames();
+}
