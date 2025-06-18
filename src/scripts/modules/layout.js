8e1f22a63a5b12d73e4c7acc0e1a89c56cf059b4
ray24004 <wraymundo@byupathway.edu>
2025-06-18 00:16:46 -0300

    Run format

--- src/scripts/modules/layout.js
+++ src/scripts/modules/layout.js
@@ -5,25 +5,25 @@ initMenu();
 initFooter();
 
 async function injectPartial(partialName, targetElement) {
-    const partialFetch = await fetch(`/partials/${partialName}.html`);
-    targetElement.innerHTML = await partialFetch.text();
+  const partialFetch = await fetch(`/partials/${partialName}.html`);
+  targetElement.innerHTML = await partialFetch.text();
 }
 
 function initMenu() {
-    const button = document.querySelector('#header button');
-    const nav = document.querySelector('#header nav');
-    const body = document.querySelector('body');
+  const button = document.querySelector('#header button');
+  const nav = document.querySelector('#header nav');
+  const body = document.querySelector('body');
 
-    button.addEventListener('click', ev => {
-        nav.classList.toggle('hidden');
-        body.classList.toggle('no-scroll')
-        
-        const isHidden = nav.classList.contains('hidden');
-        button.textContent = isHidden ? '☰' : 'X';
-    });
+  button.addEventListener('click', (ev) => {
+    nav.classList.toggle('hidden');
+    body.classList.toggle('no-scroll');
+
+    const isHidden = nav.classList.contains('hidden');
+    button.textContent = isHidden ? '☰' : 'X';
+  });
 }
 
 function initFooter() {
-    const copyYear = document.querySelector('#copyYear');
-    copyYear.textContent = new Date().getFullYear();
+  const copyYear = document.querySelector('#copyYear');
+  copyYear.textContent = new Date().getFullYear();
 }
