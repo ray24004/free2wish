f0d462af431c7eae98181551294fc3ff8d1a407b
ray24004 <wraymundo@byupathway.edu>
2025-06-18 00:16:06 -0300

    Add menu interaction; Block scrolling when menu is open; Initialize footer;

--- src/scripts/modules/layout.js
+++ src/scripts/modules/layout.js
@@ -1,7 +1,29 @@
+await injectPartial('header', document.querySelector('#header'));
+await injectPartial('footer', document.querySelector('#footer'));
+
+initMenu();
+initFooter();
+
 async function injectPartial(partialName, targetElement) {
-    let partialFetch = await fetch(`/partials/${partialName}.html`);
+    const partialFetch = await fetch(`/partials/${partialName}.html`);
     targetElement.innerHTML = await partialFetch.text();
 }
 
-injectPartial('header', document.querySelector('#header'));
-injectPartial('footer', document.querySelector('#footer'));
+function initMenu() {
+    const button = document.querySelector('#header button');
+    const nav = document.querySelector('#header nav');
+    const body = document.querySelector('body');
+
+    button.addEventListener('click', ev => {
+        nav.classList.toggle('hidden');
+        body.classList.toggle('no-scroll')
+        
+        const isHidden = nav.classList.contains('hidden');
+        button.textContent = isHidden ? '☰' : 'X';
+    });
+}
+
+function initFooter() {
+    const copyYear = document.querySelector('#copyYear');
+    copyYear.textContent = new Date().getFullYear();
+}
